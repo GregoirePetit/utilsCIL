98888e015fe7525bef50a212c99c96f522ac6b05
GregoirePetit <g.petit360@gmail.com>
2022-12-19 18:02:42 +0100

    add multilabel

--- MyFeatureFolder.py
+++ MyFeatureFolder.py
@@ -29,6 +29,30 @@ class L3FeaturesDataset(Dataset):
             image = np.array([float(x) for x in f.read().split()]).reshape(256,14,14).astype(np.float32)
         return image, label
 
+class L3FeaturesDatasetFeatureMaps(Dataset):
+
+    def __init__(self, path, range_classes=None):
+        self.y = []
+        self.X = []
+        doss = os.listdir(path)
+        if range_classes:
+            doss = [f'{d}_decomposed' for d in range_classes]
+        doss = np.sort(doss)
+        for root_path in doss:
+            if '_' in root_path:
+                for file in os.listdir(os.path.join(path, root_path)):
+                    self.y.append(int(root_path.split('_')[0]))
+                    self.X.append(os.path.join(path, root_path, file))
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, index):
+        label = self.y[index]
+        with open(self.X[index], "r") as f:
+            image = np.array([float(x) for x in f.read().split()]).reshape(14,256*14).astype(np.float32)
+        return image, label
+
 class L4FeaturesDataset(Dataset):
 
     def __init__(self, path, range_classes=None):
--- MyImageFolder.py
+++ MyImageFolder.py
@@ -131,6 +131,114 @@ class ImagesListFileFolder(data.Dataset):
         fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
         return fmt_str
 
+class ImagesListFileFolderMultilabel(data.Dataset):
+    """A generic data loader where the samples are arranged in this way: ::
+        root/class_x/xxx.ext
+        root/class_x/xxy.ext
+        root/class_x/xxz.ext
+        root/class_y/123.ext
+        root/class_y/nsdf3.ext
+        root/class_y/asd932_.ext
+    Args:
+        root (string): Root directory path.
+        loader (callable): A function to load a sample given its path.
+        extensions (list[string]): A list of allowed extensions.
+        transform (callable, optional): A function/transform that takes in
+            a sample and returns a transformed version.
+            E.g, ``transforms.RandomCrop`` for images.
+        target_transform (callable, optional): A function/transform that takes
+            in the target and transforms it.
+     Attributes:
+        classes (list): List of the class names.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        samples (list): List of (sample path, class_index) tuples
+        targets (list): The class_index value for each image in the dataset
+    """
+
+
+    def __init__(self, images_list_file, transform=None, target_transform=None, return_path=False, range_classes=None, random_seed=-1, old_load=False, open_images=False):
+
+        self.return_path = return_path
+        samples = []
+        df = pd.read_csv(images_list_file, sep=' ', names=['paths','class'])
+        if old_load:
+            root_folder = ''
+        else:
+            root_folder = df['paths'].head(1)[0]
+            df = df.tail(df.shape[0] -1)
+        df.drop_duplicates()
+        # cast the class to tuple of int
+        df['class'] = df['class'].apply(lambda x: tuple(map(int, x.split(','))))
+        df = df.sort_values('class')
+        order_list = list(set(df['class'].values.tolist()))
+        # sort the list by the first item of the tuple
+        order_list.sort(key=lambda x: x[0])
+        print('*'*(len(images_list_file)+76+len(str(random_seed))))
+        print('Class order of',images_list_file,'before shuffle with seed',random_seed,': [',*order_list[:5],'...',*order_list[-5:],']')
+        deep_copy = order_list.copy()
+        if random_seed != -1:
+            np.random.seed(random_seed)
+            random.shuffle(order_list)
+            order_list_raw = np.random.permutation(len(order_list)).tolist()
+            order_list = [deep_copy[i] for i in order_list_raw]
+        print('Class order of',images_list_file,'after  shuffle with seed',random_seed,': [',*order_list[:5],'...',*order_list[-5:],']')
+        print('*'*(len(images_list_file)+76+len(str(random_seed))))
+        order_list_reverse = [deep_copy[order_list.index(i)] for i in list(set(df['class'].values.tolist()))]
+        if range_classes:
+            index_to_take = [order_list[i] for i in range_classes]
+            samples = [(os.path.join(root_folder, elt[0]),order_list_reverse[elt[1][0]]) for elt in list(map(tuple, df.loc[df['class'].isin(index_to_take)].values.tolist()))]
+            samples.sort(key=lambda x:x[1])
+            print('We pick the classes from', min(range_classes), 'to', max(range_classes), 'and we have', len(samples), 'samples')
+        else:
+            samples = [(os.path.join(root_folder, elt[0]),order_list_reverse[elt[1][0]]) for elt in list(map(tuple, df.values.tolist()))]
+            samples.sort(key=lambda x:x[1])
+            print('We have', len(samples), 'samples')
+            print("first 3 samples",samples[:3])
+        if not samples:
+            raise(RuntimeError("No image found"))
+
+        self.images_list_file = images_list_file
+        self.loader = default_loader
+        self.extensions = IMG_EXTENSIONS
+        self.classes = list(set([e[1] for e in samples]))
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+        self.imgs = [s[0] for s in samples]
+        self.transform = transform
+        self.target_transform = target_transform
+
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        if self.return_path :
+            return (sample, target), self.samples[index][0]
+        return sample, target
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        fmt_str += '    List Description: {}\n'.format(self.images_list_file)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        tmp = '    Target Transforms (if any): '
+        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
+
 
 
 
